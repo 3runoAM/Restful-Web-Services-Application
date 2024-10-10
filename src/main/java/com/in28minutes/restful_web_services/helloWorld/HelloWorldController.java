7deaa15168f791f0eba8b738785d6d18b75fa9eb
Bruno Martins <jbrunno17@gmail.com>
2024-10-10 15:19:13 -0300

    Adds a method using a path variable, the last simple method before the Social Media API is developed.

--- src/main/java/com/in28minutes/restful_web_services/helloWorld/HelloWorldController.java
+++ src/main/java/com/in28minutes/restful_web_services/helloWorld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.in28minutes.restful_web_services.helloWorld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +16,10 @@ public class HelloWorldController {
     @GetMapping("/bean")
     public HelloBean helloBean() {
         return new HelloBean("Hello Bean");
+    }
+
+    @GetMapping("/{name}")
+    public String helloUser(@PathVariable String name) {
+        return "Hello " + name;
     }
 }
